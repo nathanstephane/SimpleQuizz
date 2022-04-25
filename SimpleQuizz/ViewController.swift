3a66eed0400aed0c3d2513b27d43bfa0b49a2d3e
Nathan <nathanstephaneth@gmail.com>
2022-04-25 12:11:40 +0200

    Initial Commit

---
+++ SimpleQuizz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleQuizz
+//
+//  Created by Guest User on 25/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
